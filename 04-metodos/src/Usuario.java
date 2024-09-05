70f7c28af0aa5d275f81029b9cb7dc92300a0483
Vitor Bittencourt <vitor_bittencourt1998@hotmail.com>
2024-09-04 21:45:33 -0300

    Adicionando arquivos das aulas e desafio

---
+++ 04-metodos/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+
+        smartTv.ligar ();
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.mudarDeCanal(13);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+    }
+
+}
---
+++ Desafio/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite seu nome!");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite seu sobrenomenome!");
+        String sobrenome = scanner.next();
+
+        System.out.println("Por favor, digite a agência!");
+        String numeroAgencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta!");
+        int numeroConta = scanner.nextInt();
+
+        double saldo = 237.48;
+
+
+        System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo de " + saldo + " reais já está disponível para saque");
+    }
+}
